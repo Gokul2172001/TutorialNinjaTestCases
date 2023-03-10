655549355aab2930db4bfc8a95e9891ee38ce4b5
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-03-10 23:52:48 +0530

    Update SignUpPageTest.java

--- src/test/java/com/crm/qa/testcases/SignUpPageTest.java
+++ src/test/java/com/crm/qa/testcases/SignUpPageTest.java
@@ -1,12 +1,10 @@
 package com.crm.qa.testcases;
-
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.SignUpPage;
-
 public class SignUpPageTest extends TestBase{
 	
 	SignUpPage signUpPage;
