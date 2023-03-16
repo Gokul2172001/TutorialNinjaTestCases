1172d6f51ef97952317e240a71b2b50bc21c3178
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-03-16 23:02:12 +0530

    Update SoftwaresPageTest.java

--- src/test/java/com/crm/qa/testcases/SoftwaresPageTest.java
+++ src/test/java/com/crm/qa/testcases/SoftwaresPageTest.java
@@ -1,11 +1,9 @@
 package com.crm.qa.testcases;
-
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.CamerasPage;
 
