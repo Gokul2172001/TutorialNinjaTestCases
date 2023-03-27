755427410c413677dd96c2d9a472b2d8a0d16bfb
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-03-27 23:57:17 +0530

    Update SoftwaresPageTest.java

--- src/test/java/com/crm/qa/testcases/SoftwaresPageTest.java
+++ src/test/java/com/crm/qa/testcases/SoftwaresPageTest.java
@@ -1,15 +1,11 @@
 package com.crm.qa.testcases;
-
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.CamerasPage;
-//import com.crm.qa.pages.LoginPage;
-//import com.crm.qa.pages.LoginPage;
 
 public class CamerasPageTest extends TestBase {
 	CamerasPage camerasPage;
