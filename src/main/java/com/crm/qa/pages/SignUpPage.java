d5c5a21cb0d6df44be1f51673c26a109ded09797
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-04-06 23:54:42 +0530

    Update SignUpPage.java

--- src/main/java/com/crm/qa/pages/SignUpPage.java
+++ src/main/java/com/crm/qa/pages/SignUpPage.java
@@ -1,14 +1,9 @@
 package com.crm.qa.pages;
-
-//import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.crm.qa.base.TestBase;
-//import com.crm.qa.util.TestUtil;
 
 public class SignUpPage extends TestBase {
 
