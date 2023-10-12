a353340d5ec4b54976aab9063d47e26de6a5c8da
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-10-12 23:57:49 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import com.crm.qa.util.TestUtil;
 
 public class TestBase {
